817f8fe317b8357aac6d11d5ebdc2ec3dd23a2a9
jossrc <kuro.rc10@gmail.com>
2020-10-20 13:38:55 -0500

    PAyuda : Avanzando un poco con el diseño del panel y configurando el año dinámico

--- src/vista/PAyuda.java
+++ src/vista/PAyuda.java
@@ -1,6 +1,9 @@
 package vista;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.util.Calendar;
 
 public class PAyuda extends JPanel {
 
@@ -13,7 +16,23 @@ public class PAyuda extends JPanel {
 		pContenidoAyuda.setBounds(0, 0, 828, 662);
 		add(pContenidoAyuda);
 		pContenidoAyuda.setLayout(null);
+		
+		JLabel lblVersion = new JLabel("Versi\u00F3n 1.0.1");
+		lblVersion.setFont(new Font("SansSerif", Font.BOLD, 26));
+		lblVersion.setBounds(335, 11, 158, 34);
+		pContenidoAyuda.add(lblVersion);
+		
+		JLabel lblDerechos = new JLabel("Todos los derechos reservados \u00A9 -");
+		lblDerechos.setFont(new Font("SansSerif", Font.ITALIC, 13));
+		lblDerechos.setBounds(314, 637, 203, 18);
+		pContenidoAyuda.add(lblDerechos);
+		
+		Calendar cal = Calendar.getInstance();		
+		JLabel lblYear = new JLabel();
+		lblYear.setFont(new Font("SansSerif", Font.ITALIC, 13));
+		lblYear.setText(cal.get(Calendar.YEAR)+"");
+		lblYear.setBounds(521, 637, 28, 18);
+		pContenidoAyuda.add(lblYear);
 
 	}
-
 }
