92142a2a57fd0cb62ad1d63ea6864e25e9d68a33
jossrc <kuro.rc10@gmail.com>
2020-10-20 11:15:35 -0500

    pMantenimiento : Aplicando estilos al submenu

--- src/vista/pMantenimiento.java
+++ src/vista/pMantenimiento.java
@@ -3,6 +3,8 @@ package vista;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
 
 public class pMantenimiento extends JPanel {
 
@@ -18,16 +20,38 @@ public class pMantenimiento extends JPanel {
 		pSubmenu.setLayout(null);
 		
 		JButton btnCliente = new JButton("Cliente");
+		btnCliente.setForeground(Color.WHITE);
+		btnCliente.setFont(new Font("SansSerif", Font.BOLD, 14));
+		btnCliente.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnCliente.setContentAreaFilled(false);
+		btnCliente.setOpaque(true);
+		btnCliente.setBorder(null);
+		btnCliente.setBackground(Color.LIGHT_GRAY);
 		btnCliente.setBounds(102, 11, 117, 29);
 		pSubmenu.add(btnCliente);
 		
 		JButton btnVentas = new JButton("Ventas");
+		btnVentas.setForeground(Color.WHITE);
+		btnVentas.setFont(new Font("SansSerif", Font.BOLD, 14));
+		btnVentas.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnVentas.setContentAreaFilled(false);
+		btnVentas.setOpaque(true);
+		btnVentas.setBorder(null);
+		btnVentas.setBackground(Color.LIGHT_GRAY);		
 		btnVentas.setBounds(303, 11, 117, 29);
 		pSubmenu.add(btnVentas);
 		
 		JButton btnProductos = new JButton("Productos");
+		btnProductos.setFont(new Font("SansSerif", Font.BOLD, 14));
+		btnProductos.setForeground(Color.WHITE);
+		btnProductos.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnProductos.setContentAreaFilled(false);
+		btnProductos.setOpaque(true);
+		btnProductos.setBorder(null);
+		btnProductos.setBackground(Color.LIGHT_GRAY);
 		btnProductos.setBounds(504, 11, 117, 29);
 		pSubmenu.add(btnProductos);
 
 	}
+
 }
