081eb5be6332b4825ff9c9b1bd8404f0a197a669
jossrc <kuro.rc10@gmail.com>
2020-10-20 12:50:44 -0500

    vista : Creando un nuevo panel principal -> pAyuda

---
+++ src/vista/PAyuda.java
@@ -0,0 +1,19 @@
+package vista;
+
+import javax.swing.JPanel;
+
+public class PAyuda extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public PAyuda() {
+		setLayout(null);
+		
+		JPanel pContenidoAyuda = new JPanel();
+		pContenidoAyuda.setBounds(0, 0, 828, 662);
+		add(pContenidoAyuda);
+		pContenidoAyuda.setLayout(null);
+
+	}
+
+}
