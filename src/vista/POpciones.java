0acd0aa75da3055c890ed02dcd3c7fe7a27aad66
jossrc <kuro.rc10@gmail.com>
2020-10-20 12:09:41 -0500

    vista : Creando un nuevo panel principal -> POpciones

---
+++ src/vista/POpciones.java
@@ -0,0 +1,14 @@
+package vista;
+
+import javax.swing.JPanel;
+
+public class POpciones extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public POpciones() {
+		setLayout(null);
+
+	}
+
+}
