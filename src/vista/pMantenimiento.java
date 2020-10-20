ce7f1a30c5746866aefdd3e4331293e986f79ab0
jossrc <kuro.rc10@gmail.com>
2020-10-20 10:03:16 -0500

    pMantenimiento : Intentando crear un submenu

--- src/vista/pMantenimiento.java
+++ src/vista/pMantenimiento.java
@@ -1,6 +1,8 @@
 package vista;
 
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Color;
 
 public class pMantenimiento extends JPanel {
 
@@ -8,7 +10,24 @@ public class pMantenimiento extends JPanel {
 
 	public pMantenimiento() {
 		setLayout(null);
+		
+		JPanel pSubmenu = new JPanel();
+		pSubmenu.setBackground(Color.LIGHT_GRAY);
+		pSubmenu.setBounds(0, 0, 724, 51);
+		add(pSubmenu);
+		pSubmenu.setLayout(null);
+		
+		JButton btnCliente = new JButton("Cliente");
+		btnCliente.setBounds(102, 11, 117, 29);
+		pSubmenu.add(btnCliente);
+		
+		JButton btnVentas = new JButton("Ventas");
+		btnVentas.setBounds(303, 11, 117, 29);
+		pSubmenu.add(btnVentas);
+		
+		JButton btnProductos = new JButton("Productos");
+		btnProductos.setBounds(504, 11, 117, 29);
+		pSubmenu.add(btnProductos);
 
 	}
-
 }
