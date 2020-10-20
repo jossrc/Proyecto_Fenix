af453cbdc344335be800f0a5c627135dfe197d07
jossrc <kuro.rc10@gmail.com>
2020-10-20 11:45:38 -0500

    vista : Creando un nuevo panel principal -> PReporte

---
+++ src/vista/PReporte.java
@@ -0,0 +1,14 @@
+package vista;
+
+import javax.swing.JPanel;
+
+public class PReporte extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public PReporte() {
+		setLayout(null);
+
+	}
+
+}
