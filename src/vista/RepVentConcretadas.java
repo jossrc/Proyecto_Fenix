ab4b82c66e397cb3a24bd616f839dc0ba0e513a9
jossrc <kuro.rc10@gmail.com>
2020-10-29 14:39:07 -0500

    vista : Creando el panel Ventas Concretadas

---
+++ src/vista/RepVentConcretadas.java
@@ -0,0 +1,18 @@
+package vista;
+
+import javax.swing.JPanel;
+
+public class RepVentConcretadas extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public RepVentConcretadas() {
+		setLayout(null);
+		
+		JPanel panelVentasConcretadas = new JPanel();
+		panelVentasConcretadas.setBounds(0, 0, 817, 551);
+		add(panelVentasConcretadas);
+		panelVentasConcretadas.setLayout(null);
+
+	}
+}
