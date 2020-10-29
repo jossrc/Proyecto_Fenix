4ddaa636486baee5e888581f34ba37af7a39e911
jossrc <kuro.rc10@gmail.com>
2020-10-29 10:35:41 -0500

    MantVentas : Creando el pDatosProducto

--- src/vista/MantVentas.java
+++ src/vista/MantVentas.java
@@ -19,6 +19,11 @@ public class MantVentas extends JPanel {
 	private static final long serialVersionUID = 1L;
 	private JTextField txtCliente;
 	private JTextField txtBoleta;
+	private JTextField txtCodigo;
+	private JTextField txtCantidad;
+	private JTextField txtPrecio;
+	private JTextField txtDescripcionProducto;
+	private JTextField txtStock;
 
 	public MantVentas() {
 		setLayout(null);
@@ -64,8 +69,63 @@ public class MantVentas extends JPanel {
 		pDatosVenta.add(txtFecha);
 		
 		JButton btnBuscarCliente = new JButton("");
-		btnBuscarCliente.setBounds(250, 44, 32, 23);
+		btnBuscarCliente.setBounds(250, 44, 26, 20);
 		pDatosVenta.add(btnBuscarCliente);
+		
+		JPanel pDatosProducto = new JPanel();
+		pDatosProducto.setBorder(crearBordeTitulo("Datos del Producto"));
+		pDatosProducto.setBounds(10, 115, 627, 132);
+		panelVentas.add(pDatosProducto);
+		pDatosProducto.setLayout(null);
+		
+		JLabel lblCodigo = new JLabel("CODIGO");
+		lblCodigo.setBounds(22, 19, 41, 14);
+		pDatosProducto.add(lblCodigo);
+		
+		txtCodigo = new JTextField();
+		txtCodigo.setColumns(10);
+		txtCodigo.setBounds(21, 44, 219, 20);
+		pDatosProducto.add(txtCodigo);
+		
+		JButton btnBuscarProducto = new JButton("");
+		btnBuscarProducto.setBounds(250, 44, 26, 20);
+		pDatosProducto.add(btnBuscarProducto);
+		
+		JLabel lblCantidad = new JLabel("CANTIDAD");
+		lblCantidad.setBounds(317, 19, 52, 14);
+		pDatosProducto.add(lblCantidad);
+		
+		txtCantidad = new JTextField();
+		txtCantidad.setBounds(317, 44, 115, 20);
+		pDatosProducto.add(txtCantidad);
+		txtCantidad.setColumns(10);
+		
+		JLabel lblPrecio = new JLabel("PRECIO");
+		lblPrecio.setBounds(479, 19, 38, 14);
+		pDatosProducto.add(lblPrecio);
+		
+		txtPrecio = new JTextField();
+		txtPrecio.setColumns(10);
+		txtPrecio.setBounds(473, 44, 115, 20);
+		pDatosProducto.add(txtPrecio);
+		
+		JLabel lblDescripcionProducto = new JLabel("DESCRIPCI\u00D3N");
+		lblDescripcionProducto.setBounds(22, 75, 69, 14);
+		pDatosProducto.add(lblDescripcionProducto);
+		
+		txtDescripcionProducto = new JTextField();
+		txtDescripcionProducto.setBounds(22, 100, 218, 20);
+		pDatosProducto.add(txtDescripcionProducto);
+		txtDescripcionProducto.setColumns(10);
+		
+		JLabel lblStock = new JLabel("STOCK");
+		lblStock.setBounds(483, 75, 33, 14);
+		pDatosProducto.add(lblStock);
+		
+		txtStock = new JTextField();
+		txtStock.setColumns(10);
+		txtStock.setBounds(473, 100, 115, 20);
+		pDatosProducto.add(txtStock);
 
 	}
 	
