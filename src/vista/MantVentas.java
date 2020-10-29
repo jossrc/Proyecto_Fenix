2b1547664564f60db2abad7d60c80cae3f9e0cd2
jossrc <kuro.rc10@gmail.com>
2020-10-29 09:57:54 -0500

    MantVentas: Creando un panel para empezar a diseñar

--- src/vista/MantVentas.java
+++ src/vista/MantVentas.java
@@ -7,7 +7,12 @@ public class MantVentas extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	public MantVentas() {
+		setLayout(null);
+		
+		JPanel panelVentas = new JPanel();
+		panelVentas.setBounds(0, 0, 817, 470);
+		add(panelVentas);
+		panelVentas.setLayout(null);
 
 	}
-
 }
