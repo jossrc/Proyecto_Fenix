125f52a16182d0861c4c6c4c5fc6a4558d5a36af
jossrc <kuro.rc10@gmail.com>
2020-10-29 10:18:58 -0500

    MantVentas : Creando un borde con titulo al pDatosVenta

--- src/vista/MantVentas.java
+++ src/vista/MantVentas.java
@@ -4,13 +4,20 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.border.TitledBorder;
+
 import com.toedter.calendar.JDateChooser;
+
+import java.awt.Color;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 
 public class MantVentas extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	private JTextField textField;
+	private JTextField txtCliente;
 	private JTextField txtBoleta;
 
 	public MantVentas() {
@@ -19,45 +26,53 @@ public class MantVentas extends JPanel {
 		JPanel panelVentas = new JPanel();
 		panelVentas.setBounds(0, 0, 817, 470);
 		add(panelVentas);
-		panelVentas.setLayout(null);
+		panelVentas.setLayout(null);		
 		
 		JPanel pDatosVenta = new JPanel();
-		pDatosVenta.setBounds(10, 11, 634, 75);
+		pDatosVenta.setBorder(crearBordeTitulo("Datos de venta"));
+		pDatosVenta.setBounds(10, 11, 627, 79);
 		panelVentas.add(pDatosVenta);
 		pDatosVenta.setLayout(null);
 		
 		JLabel lblCliente = new JLabel("CLIENTE");
-		lblCliente.setBounds(20, 11, 46, 14);
+		lblCliente.setBounds(21, 19, 46, 14);
 		pDatosVenta.add(lblCliente);
 		
-		textField = new JTextField();
-		textField.setBounds(20, 36, 219, 20);
-		pDatosVenta.add(textField);
-		textField.setColumns(10);
+		txtCliente = new JTextField();
+		txtCliente.setBounds(21, 44, 219, 20);
+		pDatosVenta.add(txtCliente);
+		txtCliente.setColumns(10);
 		
 		JLabel lblDocumento = new JLabel("DOCUMENTO");
-		lblDocumento.setBounds(312, 11, 64, 14);
+		lblDocumento.setBounds(313, 19, 64, 14);
 		pDatosVenta.add(lblDocumento);
 		
 		txtBoleta = new JTextField();
 		txtBoleta.setHorizontalAlignment(SwingConstants.CENTER);
 		txtBoleta.setText("BOLETA");
 		txtBoleta.setEditable(false);
-		txtBoleta.setBounds(312, 36, 119, 20);
+		txtBoleta.setBounds(313, 44, 119, 20);
 		pDatosVenta.add(txtBoleta);
 		txtBoleta.setColumns(10);
 		
 		JLabel lblFecha = new JLabel("FECHA");
-		lblFecha.setBounds(473, 11, 33, 14);
+		lblFecha.setBounds(474, 19, 33, 14);
 		pDatosVenta.add(lblFecha);
 		
-		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(466, 36, 128, 20);
-		pDatosVenta.add(dateChooser);
+		JDateChooser txtFecha = new JDateChooser();
+		txtFecha.setBounds(467, 44, 128, 20);
+		pDatosVenta.add(txtFecha);
 		
 		JButton btnBuscarCliente = new JButton("");
-		btnBuscarCliente.setBounds(249, 35, 32, 23);
+		btnBuscarCliente.setBounds(250, 44, 32, 23);
 		pDatosVenta.add(btnBuscarCliente);
 
+	}
+	
+	private TitledBorder crearBordeTitulo(String titulo) {
+		TitledBorder titled = new TitledBorder(UIManager.getBorder("TitledBorder.border"), titulo, TitledBorder.LEADING, TitledBorder.TOP, null, null);
+		titled.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		
+		return titled;
 	}
 }
