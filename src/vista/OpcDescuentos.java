7f4fe23f0e5a276b5beea5502349ca0358f91573
jossrc <kuro.rc10@gmail.com>
2020-10-29 16:42:41 -0500

    vista : Creando el panel OpcDescuentos

---
+++ src/vista/OpcDescuentos.java
@@ -0,0 +1,19 @@
+package vista;
+
+import javax.swing.JPanel;
+
+public class OpcDescuentos extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public OpcDescuentos() {
+		setLayout(null);
+		
+		JPanel panelDescuentos = new JPanel();
+		panelDescuentos.setBounds(0, 0, 827, 570);
+		add(panelDescuentos);
+		panelDescuentos.setLayout(null);
+
+	}
+
+}
