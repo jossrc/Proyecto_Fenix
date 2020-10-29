2728c2742aa408470c8650d671106052c8373a69
jossrc <kuro.rc10@gmail.com>
2020-10-29 14:49:49 -0500

    RepVentConcretadas : Creando el panel pBusquedasFecha

--- src/vista/RepVentConcretadas.java
+++ src/vista/RepVentConcretadas.java
@@ -1,10 +1,20 @@
 package vista;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import com.toedter.calendar.JDateChooser;
+import javax.swing.JTextField;
+
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Font;
+
+import javax.swing.JButton;
 
 public class RepVentConcretadas extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JTextField txtCantidadEncontrada;
 
 	public RepVentConcretadas() {
 		setLayout(null);
@@ -13,6 +23,48 @@ public class RepVentConcretadas extends JPanel {
 		panelVentasConcretadas.setBounds(0, 0, 817, 551);
 		add(panelVentasConcretadas);
 		panelVentasConcretadas.setLayout(null);
+		
+		JPanel pBusquedasFecha = new JPanel();
+		pBusquedasFecha.setBounds(21, 28, 544, 125);
+		panelVentasConcretadas.add(pBusquedasFecha);
+		pBusquedasFecha.setLayout(null);
+		
+		JLabel lblDesde = new JLabel("DESDE");
+		lblDesde.setBounds(10, 9, 32, 14);
+		pBusquedasFecha.add(lblDesde);
+		
+		JDateChooser txtDesde = new JDateChooser();
+		txtDesde.setBounds(10, 34, 139, 20);
+		pBusquedasFecha.add(txtDesde);
+		
+		JLabel lblHasta = new JLabel("HASTA");
+		lblHasta.setBounds(10, 65, 33, 14);
+		pBusquedasFecha.add(lblHasta);
+		
+		JDateChooser txtHasta = new JDateChooser();
+		txtHasta.setBounds(10, 90, 139, 20);
+		pBusquedasFecha.add(txtHasta);
+		
+		JLabel lblCantidadEncontrada = new JLabel("CANTIDAD ENCONTRADA");
+		lblCantidadEncontrada.setBounds(205, 9, 124, 14);
+		pBusquedasFecha.add(lblCantidadEncontrada);
+		
+		txtCantidadEncontrada = new JTextField();
+		txtCantidadEncontrada.setEditable(false);
+		txtCantidadEncontrada.setBounds(205, 34, 162, 20);
+		pBusquedasFecha.add(txtCantidadEncontrada);
+		txtCantidadEncontrada.setColumns(10);
+		
+		JButton btnBuscarVentas = new JButton("BUSCAR");
+		btnBuscarVentas.setForeground(Color.WHITE);
+		btnBuscarVentas.setFont(new Font("SansSerif", Font.BOLD, 14));
+		btnBuscarVentas.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnBuscarVentas.setContentAreaFilled(false);
+		btnBuscarVentas.setOpaque(true);
+		btnBuscarVentas.setBorder(null);
+		btnBuscarVentas.setBackground(new Color(9, 168, 136));
+		btnBuscarVentas.setBounds(205, 79, 162, 31);
+		pBusquedasFecha.add(btnBuscarVentas);
 
 	}
 }
