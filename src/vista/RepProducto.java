305936fd831d70bfe82e4715414df7a20f69a4ef
jossrc <kuro.rc10@gmail.com>
2020-11-14 17:24:08 -0500

    vista : Creando el RepProducto

---
+++ src/vista/RepProducto.java
@@ -0,0 +1,15 @@
+package vista;
+
+import javax.swing.JPanel;
+
+public class RepProducto extends JPanel {
+
+
+	private static final long serialVersionUID = 1L;
+
+	public RepProducto() {
+		setLayout(null);
+
+	}
+
+}
