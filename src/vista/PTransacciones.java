86e7dbd4e66e5b76294dd559bf000a846fecfb2d
jossrc <kuro.rc10@gmail.com>
2020-11-14 15:11:42 -0500

    PTransacciones : Avanzando con el diseño del panel

--- src/vista/PTransacciones.java
+++ src/vista/PTransacciones.java
@@ -2,11 +2,18 @@ package vista;
 
 import javax.swing.JPanel;
 
+import java.awt.Color;
+
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.CardLayout;
+
+
 public class PTransacciones extends JPanel {
 
-	/**
-	 * Create the panel.
-	 */
+	private static final long serialVersionUID = 1L;
+
+	
 	public PTransacciones() {
 		setLayout(null);
 		
@@ -14,6 +21,28 @@ public class PTransacciones extends JPanel {
 		pContenido.setBounds(0, 0, 827, 621);
 		add(pContenido);
 		pContenido.setLayout(null);
+		
+		JPanel pSubmenu = new JPanel();
+		pSubmenu.setLayout(null);
+		pSubmenu.setBackground(Color.LIGHT_GRAY);
+		pSubmenu.setBounds(0, 0, 827, 51);
+		pContenido.add(pSubmenu);
+		
+		JButton btnVentas = new JButton("Ventas");
+		btnVentas.setOpaque(true);
+		btnVentas.setForeground(Color.WHITE);
+		btnVentas.setFont(new Font("SansSerif", Font.BOLD, 14));
+		btnVentas.setContentAreaFilled(false);
+		btnVentas.setBorder(null);
+		btnVentas.setBackground(Color.LIGHT_GRAY);
+		btnVentas.setBounds(340, 11, 117, 29);
+		pSubmenu.add(btnVentas);
+		
+		JPanel pContenidoVentas = new JPanel();
+		pContenidoVentas.setBounds(0, 51, 827, 570);
+		pContenido.add(pContenidoVentas);
+		pContenidoVentas.setLayout(new CardLayout(0, 0));
 
 	}
+	
 }
