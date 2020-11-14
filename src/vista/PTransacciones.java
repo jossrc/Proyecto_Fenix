b61d4d581b5b06e575ae65eb985e4b56409ba436
jossrc <kuro.rc10@gmail.com>
2020-11-14 15:18:47 -0500

    Haciendo que el PTransacciones habra por defecto el TRVentas

--- src/vista/PTransacciones.java
+++ src/vista/PTransacciones.java
@@ -2,16 +2,23 @@ package vista;
 
 import javax.swing.JPanel;
 
+import util.CambiaPanel;
+
 import java.awt.Color;
 
 import javax.swing.JButton;
 import java.awt.Font;
 import java.awt.CardLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class PTransacciones extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	private JPanel pContenidoVentas;
+	private JPanel pSubmenu;
+	private JButton btnVentas;
 
 	
 	public PTransacciones() {
@@ -22,13 +29,13 @@ public class PTransacciones extends JPanel {
 		add(pContenido);
 		pContenido.setLayout(null);
 		
-		JPanel pSubmenu = new JPanel();
+		pSubmenu = new JPanel();
 		pSubmenu.setLayout(null);
 		pSubmenu.setBackground(Color.LIGHT_GRAY);
 		pSubmenu.setBounds(0, 0, 827, 51);
 		pContenido.add(pSubmenu);
 		
-		JButton btnVentas = new JButton("Ventas");
+		btnVentas = new JButton("Ventas");		
 		btnVentas.setOpaque(true);
 		btnVentas.setForeground(Color.WHITE);
 		btnVentas.setFont(new Font("SansSerif", Font.BOLD, 14));
@@ -38,11 +45,23 @@ public class PTransacciones extends JPanel {
 		btnVentas.setBounds(340, 11, 117, 29);
 		pSubmenu.add(btnVentas);
 		
-		JPanel pContenidoVentas = new JPanel();
+		pContenidoVentas = new JPanel();
 		pContenidoVentas.setBounds(0, 51, 827, 570);
 		pContenido.add(pContenidoVentas);
 		pContenidoVentas.setLayout(new CardLayout(0, 0));
+		
+		btnVentas.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				new CambiaPanel(pContenidoVentas, new TRVentas());
+			}
+		});
+		
+		activarVentasDefault();
 
+	}
+	
+	private void activarVentasDefault() {
+		new CambiaPanel(pContenidoVentas, new TRVentas());
 	}
 	
 }
