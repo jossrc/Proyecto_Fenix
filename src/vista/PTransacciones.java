21fc429f31164378383e86e908a216deae40efe1
jossrc <kuro.rc10@gmail.com>
2020-11-14 15:03:11 -0500

    vista : Creando el panel Principal PTransacciones

---
+++ src/vista/PTransacciones.java
@@ -0,0 +1,19 @@
+package vista;
+
+import javax.swing.JPanel;
+
+public class PTransacciones extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public PTransacciones() {
+		setLayout(null);
+		
+		JPanel pContenido = new JPanel();
+		pContenido.setBounds(0, 0, 827, 621);
+		add(pContenido);
+		pContenido.setLayout(null);
+
+	}
+}
