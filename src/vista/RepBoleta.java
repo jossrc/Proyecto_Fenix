abf43e020f4b94e3c2d97962f082a456fc97c167
jossrc <kuro.rc10@gmail.com>
2020-11-14 16:39:45 -0500

    vista : Creando el RepBoleta

---
+++ src/vista/RepBoleta.java
@@ -0,0 +1,19 @@
+package vista;
+
+import javax.swing.JPanel;
+
+public class RepBoleta extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public RepBoleta() {
+		setLayout(null);
+		
+		JPanel pBoleta = new JPanel();
+		pBoleta.setBounds(0, 0, 827, 570);
+		add(pBoleta);
+		pBoleta.setLayout(null);
+
+	}
+
+}
