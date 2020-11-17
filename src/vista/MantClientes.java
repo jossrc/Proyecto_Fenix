84afa52dbebd6836312d7121337e8e7403b7f0ef
jossrc <kuro.rc10@gmail.com>
2020-11-16 19:02:34 -0500

    MantClientes : Validando los txt y creando los metodos necesarios

--- src/vista/MantClientes.java
+++ src/vista/MantClientes.java
@@ -2,6 +2,7 @@ package vista;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
@@ -12,11 +13,10 @@ import java.awt.Cursor;
 import java.awt.Font;
 import javax.swing.JTable;
 
-
 public class MantClientes extends JPanel {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private JTextField txtNombres;
 	private JTextField txtApellidos;
 	private JTextField txtDNI;
@@ -24,69 +24,68 @@ public class MantClientes extends JPanel {
 	private JTextField txtDireccion;
 	private JTable tblCliente;
 
-	
 	public MantClientes() {
 		setLayout(null);
-		
-		//Dando Forma al Panel
+
+		// Dando Forma al Panel
 		JPanel panelClientes = new JPanel();
 		panelClientes.setBounds(0, 0, 817, 470);
 		add(panelClientes);
 		panelClientes.setLayout(null);
-		
+
 		JLabel lblNombres = new JLabel("Nombres");
-		lblNombres.setBounds(37, 45, 46, 14);
+		lblNombres.setBounds(37, 67, 46, 14);
 		panelClientes.add(lblNombres);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 187, 600, 271);
 		panelClientes.add(scrollPane);
-		
+
 		tblCliente = new JTable();
 		scrollPane.setViewportView(tblCliente);
-		
+
 		JLabel lblApellidos = new JLabel("Apellidos");
-		lblApellidos.setBounds(37, 70, 46, 14);
+		lblApellidos.setBounds(37, 92, 46, 14);
 		panelClientes.add(lblApellidos);
-		
+
 		JLabel lblDni = new JLabel("DNI");
-		lblDni.setBounds(37, 95, 46, 14);
+		lblDni.setBounds(37, 39, 46, 14);
 		panelClientes.add(lblDni);
-		
+
 		JLabel lblTelefono = new JLabel("Tel\u00E9fono");
 		lblTelefono.setBounds(37, 120, 46, 14);
 		panelClientes.add(lblTelefono);
-		
+
 		JLabel lblDireccin = new JLabel("Direcci\u00F3n");
 		lblDireccin.setBounds(37, 145, 46, 14);
 		panelClientes.add(lblDireccin);
-		
+
 		txtNombres = new JTextField();
-		txtNombres.setBounds(109, 42, 323, 20);
+		txtNombres.setBounds(109, 64, 323, 20);
 		panelClientes.add(txtNombres);
 		txtNombres.setColumns(10);
-		
+
 		txtApellidos = new JTextField();
-		txtApellidos.setBounds(109, 67, 323, 20);
+		txtApellidos.setBounds(109, 89, 323, 20);
 		txtApellidos.setColumns(10);
 		panelClientes.add(txtApellidos);
-		
+
 		txtDNI = new JTextField();
-		txtDNI.setBounds(109, 92, 323, 20);
+		txtDNI.setBounds(109, 36, 323, 20);
 		txtDNI.setColumns(10);
 		panelClientes.add(txtDNI);
-		
+
 		txtTelefono = new JTextField();
 		txtTelefono.setBounds(109, 117, 323, 20);
 		txtTelefono.setColumns(10);
 		panelClientes.add(txtTelefono);
-		
+
 		txtDireccion = new JTextField();
 		txtDireccion.setBounds(109, 142, 323, 20);
 		txtDireccion.setColumns(10);
 		panelClientes.add(txtDireccion);
-		
-		JButton btnLimpiar = new JButton("Limpiar");
+
+		JButton btnLimpiar = new JButton("Limpiar");		
 		btnLimpiar.setBounds(477, 45, 122, 53);
 		btnLimpiar.setForeground(Color.WHITE);
 		btnLimpiar.setFont(new Font("SansSerif", Font.BOLD, 14));
@@ -96,7 +95,7 @@ public class MantClientes extends JPanel {
 		btnLimpiar.setBorder(null);
 		btnLimpiar.setBackground(Color.LIGHT_GRAY);
 		panelClientes.add(btnLimpiar);
-		
+
 		JButton btnVerTodo = new JButton("Ver Todo");
 		btnVerTodo.setBounds(477, 106, 122, 53);
 		btnVerTodo.setForeground(Color.WHITE);
@@ -106,12 +105,8 @@ public class MantClientes extends JPanel {
 		btnVerTodo.setOpaque(true);
 		btnVerTodo.setBorder(null);
 		btnVerTodo.setBackground(Color.LIGHT_GRAY);
-		btnVerTodo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
 		panelClientes.add(btnVerTodo);
-		
+
 		JButton btnAgregar = new JButton("Agregar");
 		btnAgregar.setBounds(633, 189, 122, 53);
 		btnAgregar.setForeground(Color.WHITE);
@@ -122,7 +117,7 @@ public class MantClientes extends JPanel {
 		btnAgregar.setBorder(null);
 		btnAgregar.setBackground(Color.LIGHT_GRAY);
 		panelClientes.add(btnAgregar);
-		
+
 		JButton btnEditar = new JButton("Editar");
 		btnEditar.setBounds(633, 253, 122, 53);
 		btnEditar.setForeground(Color.WHITE);
@@ -133,7 +128,7 @@ public class MantClientes extends JPanel {
 		btnEditar.setBorder(null);
 		btnEditar.setBackground(Color.LIGHT_GRAY);
 		panelClientes.add(btnEditar);
-		
+
 		JButton btnBuscar = new JButton("Buscar");
 		btnBuscar.setBounds(633, 317, 122, 53);
 		btnBuscar.setForeground(Color.WHITE);
@@ -144,7 +139,7 @@ public class MantClientes extends JPanel {
 		btnBuscar.setBorder(null);
 		btnBuscar.setBackground(Color.LIGHT_GRAY);
 		panelClientes.add(btnBuscar);
-		
+
 		JButton btnEliminar = new JButton("Eliminar");
 		btnEliminar.setBounds(633, 381, 122, 53);
 		btnEliminar.setForeground(Color.WHITE);
@@ -155,10 +150,147 @@ public class MantClientes extends JPanel {
 		btnEliminar.setBorder(null);
 		btnEliminar.setBackground(Color.LIGHT_GRAY);
 		panelClientes.add(btnEliminar);
-		
+
 		JLabel lblClientes = new JLabel("Clientes");
 		lblClientes.setBounds(109, 11, 78, 14);
 		panelClientes.add(lblClientes);
 
+		btnAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				crearCliente();
+			}
+		});
+		
+		btnLimpiar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				limpiar();
+			}
+		});
+
+		btnVerTodo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
 	}
+
+	private void crearCliente() {
+		String dni = leerDNI();
+		String nombre, apellido, telefono, direccion;
+		
+		if (dni != null) {
+			nombre = leerNombre();
+			if (nombre != null) {
+				apellido = leerApellido();
+				if (apellido != null) {
+					telefono = leerTelefono();
+					if (telefono != null) {
+						direccion = leerDireccion();
+						if (direccion != null) {							
+							JOptionPane.showMessageDialog(this, "Nuevo Cliente agregado");
+							limpiar();
+							// TODO: Crea un objeto cliente y lo retorna							
+						}
+					}
+				}
+			}
+		}
+		
+		//return null
+	}
+
+	private String leerDNI() {
+
+		String dni = txtDNI.getText().trim();
+
+		if (dni.isEmpty()) {
+			aviso("El campo DNI está vacío");
+			return null;
+		}
+
+		if (!dni.matches("^[0-9]{8}$")) {
+			aviso("Ingrese un DNI válido");
+			return null;
+		}
+
+		return dni;
+
+	}
+
+	private String leerNombre() {
+		String nombre = txtNombres.getText().trim();
+
+		if (nombre.isEmpty()) {
+			aviso("El campo nombre está vacío");
+			return null;
+		}
+
+		if (!nombre.matches("^(?=.{3,15}$)[A-ZÁÉÍÓÚa-zñáéíóú]+(?:\\s[A-ZÁÉÍÓÚa-zñáéíóú]+)?$")) {
+			aviso("Ingrese un nombre válido");
+			return null;
+		}
+
+		return nombre;
+
+	}
+
+	private String leerApellido() {
+		String apellido = txtApellidos.getText().trim();
+
+		if (apellido.isEmpty()) {
+			aviso("El campo apellido está vacío");
+			return null;
+		}
+
+		if (!apellido.matches("^(?=.{3,15}$)[A-ZÁÉÍÓÚa-zñáéíóú]+(?:\\s[A-ZÁÉÍÓÚa-zñáéíóú]+)?$")) {
+			aviso("Ingrese un apellido válido");
+			return null;
+		}
+
+		return apellido;
+	}
+
+	private String leerTelefono() {
+
+		String telefono = txtTelefono.getText().trim();
+
+		if (telefono.isEmpty()) {
+			aviso("El campo Telefono está vacío");
+			return null;
+		}
+
+		if (!telefono.matches("([0-9]{7,9})")) {
+			aviso("Ingrese un teléfono válido");
+			return null;
+		}
+
+		return telefono;
+
+	}
+
+	private String leerDireccion() {
+		String direccion = txtDireccion.getText().trim();
+		if (direccion.isEmpty()) {
+			aviso("El campo Dirección está vacío");
+			return null;
+		}
+
+		return direccion;
+	}
+
+	private void aviso(String mensaje) {
+		JOptionPane.showMessageDialog(this, mensaje, "Aviso", 2);
+	}
+	
+	private void limpiar() {
+		txtDNI.setText("");
+		txtNombres.setText("");
+		txtApellidos.setText("");
+		txtTelefono.setText("");
+		txtDireccion.setText("");
+		
+		txtDNI.requestFocus();
+		txtDNI.selectAll();
+	}
+
 }
