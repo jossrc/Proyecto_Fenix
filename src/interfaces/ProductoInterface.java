c10fafb80d092fcc37ff4b67e214937ec9922269
jossrc <kuro.rc10@gmail.com>
2020-11-21 16:41:55 -0500

    ProductoInterface: Estableciendo metodos necesarios

--- src/interfaces/ProductoInterface.java
+++ src/interfaces/ProductoInterface.java
@@ -1,5 +1,19 @@
 package interfaces;
 
+import java.util.ArrayList;
+
+import model.Producto;
+
 public interface ProductoInterface {
 
+	public int registrar(Producto producto);
+	
+	public int eliminar(int codigo);
+	
+	public int actualizar(Producto producto);
+	
+	public Producto buscar(int codigo);
+	
+	public ArrayList<Producto> listado();
+	
 }
