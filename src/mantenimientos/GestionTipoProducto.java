93d2aec2c66a177ba953f0220e8b7199b6a48c83
jossrc <kuro.rc10@gmail.com>
2020-11-21 16:46:07 -0500

    src: Creando el paquete mantenimientos y la clase GestionTipoProducto

---
+++ src/mantenimientos/GestionTipoProducto.java
@@ -0,0 +1,18 @@
+package mantenimientos;
+
+import java.util.ArrayList;
+
+import interfaces.TipoProductoInterface;
+import model.Tipo_Producto;
+
+public class GestionTipoProducto implements TipoProductoInterface {
+
+	@Override
+	public ArrayList<Tipo_Producto> listado() {
+		
+		return null;
+	}
+
+	
+	
+}
