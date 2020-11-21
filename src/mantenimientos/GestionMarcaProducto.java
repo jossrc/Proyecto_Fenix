929f52a87aee4e886c99e850556c48aeef0515ae
jossrc <kuro.rc10@gmail.com>
2020-11-21 17:07:04 -0500

    Creando el mantenimiento GestionMarcaProducto

---
+++ src/mantenimientos/GestionMarcaProducto.java
@@ -0,0 +1,16 @@
+package mantenimientos;
+
+import java.util.ArrayList;
+
+import interfaces.MarcaProductoInterface;
+import model.Marca_Producto;
+
+public class GestionMarcaProducto implements MarcaProductoInterface {
+
+	@Override
+	public ArrayList<Marca_Producto> listado() {
+		
+		return null;
+	}
+
+}
