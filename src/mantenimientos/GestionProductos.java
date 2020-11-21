19656d6aadb3f84ae07200fbc3412a455e534d95
jossrc <kuro.rc10@gmail.com>
2020-11-21 17:41:57 -0500

    GestionProductos: Configurando el metodo registrar

--- src/mantenimientos/GestionProductos.java
+++ src/mantenimientos/GestionProductos.java
@@ -1,16 +1,51 @@
 package mantenimientos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import interfaces.ProductoInterface;
 import model.Producto;
+import util.MySQLConexion8;
 
 public class GestionProductos implements ProductoInterface {
 
 	@Override
-	public int registrar(Producto producto) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int registrar(Producto p) {
+		int rs = 0;
+		Connection con = null;
+		PreparedStatement pst = null;
+
+		try {
+			
+			con = MySQLConexion8.getConexion();
+			
+			String sql = "INSERT INTO PRODUCTO VALUES (?, ?, ?, ?, ?, ?, ?)";
+			
+			pst = con.prepareStatement(sql);
+			
+			pst.setString(1, p.getCodigo());
+			pst.setString(2, p.getDescripcion());
+			pst.setInt(3, p.getIdMarca());
+			pst.setInt(4, p.getIdTipo());
+			pst.setInt(5, p.getStock());
+			pst.setDouble(6, p.getPrecioUnitario());
+			pst.setInt(7, p.getEstado());
+			
+			rs = pst.executeUpdate();
+
+		} catch (Exception e) {
+			System.out.println("Error en registrar Producto : " + e.getMessage());
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("Error al cerrar : " + e.getMessage());
+			}
+		}
+
+		return rs;
 	}
 
 	@Override
