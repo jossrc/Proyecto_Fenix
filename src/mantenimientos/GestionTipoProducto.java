4be59357819323b5b6552f74bdaf7bc8020abdf8
jossrc <kuro.rc10@gmail.com>
2020-11-21 16:56:17 -0500

    GestionTipoProducto : Configurando metodo para obtener lista de tipos

--- src/mantenimientos/GestionTipoProducto.java
+++ src/mantenimientos/GestionTipoProducto.java
@@ -1,18 +1,54 @@
 package mantenimientos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import interfaces.TipoProductoInterface;
 import model.TipoProducto;
+import util.MySQLConexion8;
 
 public class GestionTipoProducto implements TipoProductoInterface {
 
 	@Override
 	public ArrayList<TipoProducto> listado() {
 		
-		return null;
+		ArrayList<TipoProducto> lista = new ArrayList<TipoProducto>();
+		Connection con = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		
+		try {
+			con = MySQLConexion8.getConexion();
+			String sql = "";
+
+			pst = con.prepareStatement(sql);		
+
+			rs = pst.executeQuery();			
+			
+			while(rs.next()) {	
+				
+				int id = rs.getInt(1);
+				String descripcion = rs.getString(2);
+				
+				TipoProducto tipo = new TipoProducto(id, descripcion);
+				
+				lista.add(tipo);
+			}
+
+		} catch (Exception e) {
+			System.out.println("Error en listado (Tipos) " + e.getMessage());
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("Error al cerrar : " + e.getMessage());
+			}
+		}
+		
+		return lista;
 	}
 
-	
-	
 }
