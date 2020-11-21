a40d7e4043744c59b8c5210b5b40d3770974091a
jossrc <kuro.rc10@gmail.com>
2020-11-21 16:36:00 -0500

    MarcaProductoInterface : Estableciendo el metodo listado

--- src/interfaces/MarcaProductoInterface.java
+++ src/interfaces/MarcaProductoInterface.java
@@ -1,5 +1,11 @@
 package interfaces;
 
+import java.util.ArrayList;
+
+import model.Marca_Producto;
+
 public interface MarcaProductoInterface {
 
+	public ArrayList<Marca_Producto> listado();
+	
 }
