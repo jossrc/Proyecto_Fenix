f8e50acf8a8a9be1b56f7e6b76bbc36e35afb3bb
jossrc <kuro.rc10@gmail.com>
2020-11-21 17:30:52 -0500

    Creando el mantenimiento para Productos

---
+++ src/mantenimientos/GestionProductos.java
@@ -0,0 +1,40 @@
+package mantenimientos;
+
+import java.util.ArrayList;
+
+import interfaces.ProductoInterface;
+import model.Producto;
+
+public class GestionProductos implements ProductoInterface {
+
+	@Override
+	public int registrar(Producto producto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int eliminar(int codigo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int actualizar(Producto producto) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Producto buscar(int codigo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Producto> listado() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
