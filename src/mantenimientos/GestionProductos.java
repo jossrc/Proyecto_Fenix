4f7d5faaf54d16dad9d50d78af33340739d9e62e
jossrc <kuro.rc10@gmail.com>
2020-11-21 19:07:33 -0500

    GestionProductos : Configurando el metodo buscar

--- src/mantenimientos/GestionProductos.java
+++ src/mantenimientos/GestionProductos.java
@@ -2,6 +2,7 @@ package mantenimientos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -89,9 +90,46 @@ public class GestionProductos implements ProductoInterface {
 	}
 
 	@Override
-	public Producto buscar(int codigo) {
-		// TODO Auto-generated method stub
-		return null;
+	public Producto buscar(int codProducto) {
+		Producto p = null;
+
+		Connection con = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+
+		try {
+			con = MySQLConexion8.getConexion();
+			String sql = "SELECT * FROM PRODUCTO WHERE COD_PRO = ?";
+			pst = con.prepareStatement(sql);
+
+			pst.setInt(1, codProducto);
+
+			rs = pst.executeQuery();
+
+			if (rs.next()) {
+								
+				String codigo = rs.getString(1);
+				String descripcion = rs.getString(2);
+				int idMarca = rs.getInt(3);
+				int idTipo = rs.getInt(4);
+				int stock = rs.getInt(5);
+				double precioUnitario = rs.getDouble(6);
+				int estado = rs.getInt(7);
+				
+				p = new Producto(codigo, descripcion, idMarca, idTipo, stock, precioUnitario, estado);
+
+			}
+		} catch (Exception e) {
+			System.out.println("Error en buscar Producto : " + e.getMessage());
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("Error al cerrar : " + e.getMessage());
+			}
+		}
+
+		return p;
 	}
 
 	@Override
