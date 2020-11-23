d240722d8e43e363eebc4c0d9efdce91e92c83cd
FunctionalHarpy <ayefimova@gmail.com>
2020-11-22 20:29:10 -0500

    Implementando interfaz de ReporteProducto con varios metodos de listado

---
+++ src/interfaces/ReporteProductoInterface.java
@@ -0,0 +1,18 @@
+package interfaces;
+
+import java.util.ArrayList;
+
+import model.ReporteProducto;
+
+public interface ReporteProductoInterface {
+	
+	public ArrayList<ReporteProducto> listado();
+	
+	public ArrayList<ReporteProducto> listadoxmarca(int codMarca);
+	
+	public ArrayList<ReporteProducto> listadoxtipo (int codTipo);
+	
+	public ArrayList<ReporteProducto> listadoxstock (int stock);
+	
+	public ArrayList<ReporteProducto> listadoxprecio (double precio);
+}
