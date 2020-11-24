e683355588983b43c78a4e387545407b6d0e30f3
jossrc <kuro.rc10@gmail.com>
2020-11-24 07:57:12 -0500

    GestionVendedores: Configurando el metodo registrar

--- src/mantenimientos/GestionVendedores.java
+++ src/mantenimientos/GestionVendedores.java
@@ -15,7 +15,35 @@ public class GestionVendedores implements VendedorInterface {
 	public int registrar(Vendedor vendedor) {
 		int rs = 0;
 
-		
+		Connection con = null;
+		PreparedStatement pst = null;
+
+		try {
+
+			con = MySQLConexion8.getConexion();
+
+			String sql = "INSERT INTO VENDEDOR VALUES (null, ?, ?, ?, ?, ?, ?)";
+
+			pst = con.prepareStatement(sql);
+
+			pst.setString(1, vendedor.getDni());
+			pst.setString(2, vendedor.getNombre());
+			pst.setString(3, vendedor.getApellido());
+			pst.setString(4, vendedor.getDireccion());
+			pst.setString(5, vendedor.getTelefono());
+			pst.setInt(6, vendedor.getEstado());
+			
+			rs = pst.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("Error en registrar vendedor : " + e.getMessage());
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 		return rs;
 	}
 
