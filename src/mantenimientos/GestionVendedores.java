ccea913bbae6d5504d8fcf7caeed539775c218c6
jossrc <kuro.rc10@gmail.com>
2020-11-24 09:19:21 -0500

    GestionVendedores: Configurando el metodo buscar

--- src/mantenimientos/GestionVendedores.java
+++ src/mantenimientos/GestionVendedores.java
@@ -2,6 +2,7 @@ package mantenimientos;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -82,8 +83,45 @@ public class GestionVendedores implements VendedorInterface {
 
 	@Override
 	public Vendedor buscar(String dni) {
-		// TODO Auto-generated method stub
-		return null;
+		Vendedor vendedor= null;
+
+		Connection con = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+
+		try {
+			con = MySQLConexion8.getConexion();
+			String sql = "SELECT * FROM VENDEDOR WHERE DNI_VEND = ? AND ESTADO = 1";
+			pst = con.prepareStatement(sql);
+
+			pst.setString(1, dni);
+
+			rs = pst.executeQuery();
+
+			if (rs.next()) {
+
+				int id = rs.getInt(1);
+				String dniVend = rs.getString(2);
+				String nombre = rs.getString(3);
+				String apellido = rs.getString(4);
+				String direccion = rs.getString(5);
+				String telefono = rs.getString(6);
+				int estado = rs.getInt(7);
+
+				vendedor = new Vendedor(id, dniVend, nombre, apellido, direccion, telefono, estado);
+
+			}
+		} catch (Exception e) {
+			System.out.println("Error en buscar Vendedor : " + e.getMessage());
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("Error al cerrar : " + e.getMessage());
+			}
+		}
+
+		return vendedor;
 	}
 
 	@Override
