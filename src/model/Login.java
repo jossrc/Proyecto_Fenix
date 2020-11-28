90e999e0c13a539b5a2a90939fa8ed8e4ac90113
jossrc <kuro.rc10@gmail.com>
2020-11-28 14:27:34 -0500

    Login: Creando atributos y metodos para

--- src/model/Login.java
+++ src/model/Login.java
@@ -2,4 +2,38 @@ package model;
 
 public class Login {
 
+	private String username;
+	private String clave;
+	private int idVendedor;
+	
+	public Login(String username, String clave, int idVendedor) {
+		this.username = username;
+		this.clave = clave;
+		this.idVendedor = idVendedor;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	public int getIdVendedor() {
+		return idVendedor;
+	}
+
+	public void setIdVendedor(int idVendedor) {
+		this.idVendedor = idVendedor;
+	}
+
 }
