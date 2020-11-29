3f583a02300e3503bce4307e6085b1164b1d47a8
jossrc <kuro.rc10@gmail.com>
2020-11-28 19:54:41 -0500

    Terminando de configurar la busqueda

--- src/vista/DlgBuscarCliente.java
+++ src/vista/DlgBuscarCliente.java
@@ -5,24 +5,43 @@ import java.awt.EventQueue;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.table.DefaultTableModel;
+
+import mantenimientos.GestionClientes;
+import model.Cliente;
+
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class DlgBuscarCliente extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private JTable tblCliente;
+	private DefaultTableModel model;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				
+				try {
+					UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+				} catch (Throwable e) {
+					e.printStackTrace();
+				}
+				
 				try {
 					DlgBuscarCliente dialog = new DlgBuscarCliente();
 					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
@@ -37,11 +56,11 @@ public class DlgBuscarCliente extends JDialog {
 	public DlgBuscarCliente() {
 		setResizable(false);
 		setTitle("BuscarCliente");
-		setBounds(100, 100, 442, 420);
+		setBounds(100, 100, 452, 436);
 		getContentPane().setLayout(null);
 		
 		JPanel pContenido = new JPanel();
-		pContenido.setBounds(0, 0, 436, 391);
+		pContenido.setBounds(0, 0, 443, 407);
 		getContentPane().add(pContenido);
 		pContenido.setLayout(null);
 		
@@ -58,23 +77,55 @@ public class DlgBuscarCliente extends JDialog {
 		scrollPane.setBounds(10, 61, 416, 286);
 		pContenido.add(scrollPane);
 		
-		tblCliente = new JTable();
+		tblCliente = new JTable();		
+		model = new DefaultTableModel();
+		tblCliente.setModel(model);
+		model.addColumn("N°");
+		model.addColumn("DNI");
+		model.addColumn("Cliente");
 		scrollPane.setViewportView(tblCliente);
 		
 		JButton btnSeleccionar = new JButton("SELECCIONAR");		
 		btnSeleccionar.setBounds(174, 357, 103, 23);
 		pContenido.add(btnSeleccionar);
 		
+		listadoClientes();
+		
 		btnSeleccionar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				
+			public void actionPerformed(ActionEvent e) {				
+				obtenerCliente();
 			}
 		});
 	}
 	
-	private void listadoClientes() {
+	private void obtenerCliente() {
+		int fila = tblCliente.getSelectedRow();
 		
+		String id = tblCliente.getValueAt(fila, 0).toString();
+		String nombreCompleto = tblCliente.getValueAt(fila, 2).toString();
+		
+		TRVentas.txtIDCliente.setText(id);
+		TRVentas.txtCliente.setText(nombreCompleto);
+		
+		dispose();
+	}
+	
+	private void listadoClientes() {
+		ArrayList<Cliente> lista = new GestionClientes().listadoDesc();
+		
+		if (lista == null){
+			JOptionPane.showMessageDialog(this, "Listado vacio");
+		} else{
+			model.setRowCount(0);
+			for (Cliente c : lista){
+				insertarNuevaFila(c);				
+			}
+		}
+	}
+	
+	private void insertarNuevaFila(Cliente c) {
+		Object datos[] = {c.getId_cli(), c.getDni_cli(), c.getNom_cli() + " " + c.getApe_cli()};
+		model.addRow(datos);
 	}
 	
 }
