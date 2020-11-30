4d2e7c4277abe7ba2f15e484796b26ec2e846851
jossrc <kuro.rc10@gmail.com>
2020-11-30 13:55:03 -0500

    Creando interface para las ventas concretadas

---
+++ src/interfaces/VentasConcretadasInterface.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+import java.util.ArrayList;
+
+import model.VentaConcretada;
+
+public interface VentasConcretadasInterface {
+
+	public int cantidadVentas(String fech1, String fech2);
+	
+	public ArrayList<VentaConcretada> buscarVentasEnFechas(String fech1, String fech2, int cantidad);
+	
+}
