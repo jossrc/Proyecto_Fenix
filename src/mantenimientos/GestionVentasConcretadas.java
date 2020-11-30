2e005cce54ce3a3baf5733e19959664bf7224952
jossrc <kuro.rc10@gmail.com>
2020-11-30 14:18:11 -0500

    Configurando el metodo de cantidadVentas

--- src/mantenimientos/GestionVentasConcretadas.java
+++ src/mantenimientos/GestionVentasConcretadas.java
@@ -14,7 +14,36 @@ public class GestionVentasConcretadas implements VentasConcretadasInterface{
 
 	@Override
 	public int cantidadVentas(String fech1, String fech2) {
-		
+		int cantidad = 0;
+
+		Connection con = null;
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+
+		try {
+			con = MySQLConexion8.getConexion();
+			String sql = "{call usp_buscarCantidadVentasEnFechas(?,?)}";
+			pst = con.prepareStatement(sql);
+
+			pst.setString(1, fech1);
+			pst.setString(2, fech2);
+
+			rs = pst.executeQuery();
+
+			if (rs.next()) {						
+				cantidad = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			System.out.println("Error en obtener la cantidad de Ventas Concretadas: " + e.getMessage());
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				System.out.println("Error al cerrar : " + e.getMessage());
+			}
+		}
+
+		return cantidad;
 	}
 
 	@Override
