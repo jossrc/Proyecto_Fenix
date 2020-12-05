0fa68725ec28242ff4319274fdeaededce90a282
jossrc <kuro.rc10@gmail.com>
2020-12-05 13:13:24 -0500

    Agregando metodos a usar

--- src/interfaces/BoletaDNIClienteInterface.java
+++ src/interfaces/BoletaDNIClienteInterface.java
@@ -4,5 +4,9 @@ import java.util.ArrayList;
 
 public interface BoletaDNIClienteInterface {
 
+public ArrayList<BoletaDNIClienteInterface> listadoPorBoleta(String boleta);
 	
+	public ArrayList<BoletaDNIClienteInterface> listadoPorDNI(String dni);
+	
+	public ArrayList<BoletaDNIClienteInterface> listadoPorRangoFechas(String fech1, String fech2);
 }
