82318dba63758ec059ff959b6a081beb58f8d9ac
jossrc <kuro.rc10@gmail.com>
2020-12-05 13:16:18 -0500

    Creando la GestionBoletaDNIClientes

---
+++ src/mantenimientos/GestionBoletaDNIClientes.java
@@ -0,0 +1,27 @@
+package mantenimientos;
+
+import java.util.ArrayList;
+
+import interfaces.BoletaDNIClienteInterface;
+
+public class GestionBoletaDNIClientes implements BoletaDNIClienteInterface {
+
+	@Override
+	public ArrayList<BoletaDNIClienteInterface> listadoPorBoleta(int boleta) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<BoletaDNIClienteInterface> listadoPorDNI(String dni) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<BoletaDNIClienteInterface> listadoPorRangoFechas(String fech1, String fech2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
