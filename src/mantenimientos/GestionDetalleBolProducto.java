2e440ce58523ce7367e752fce61c25f640dc970f
jossrc <kuro.rc10@gmail.com>
2020-12-06 14:37:51 -0500

    Creando la Gestion para el DetalleBolProducto

---
+++ src/mantenimientos/GestionDetalleBolProducto.java
@@ -0,0 +1,16 @@
+package mantenimientos;
+
+import java.util.ArrayList;
+
+import interfaces.DetalleBoletaProductoInterface;
+import model.DetalleBoleta;
+
+public class GestionDetalleBolProducto implements DetalleBoletaProductoInterface {
+
+	@Override
+	public ArrayList<DetalleBoleta> listaDetalle(int numBol) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
