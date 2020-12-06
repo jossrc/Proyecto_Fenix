1a076182b027f937ef89de08d9746a6999e576b2
jossrc <kuro.rc10@gmail.com>
2020-12-06 10:34:30 -0500

    Creando una gestion para la boleta

---
+++ src/mantenimientos/GestionBoleta.java
@@ -0,0 +1,14 @@
+package mantenimientos;
+
+import interfaces.BoletaInterface;
+import model.Boleta;
+
+public class GestionBoleta implements BoletaInterface {
+
+	@Override
+	public Boleta buscarPorNumero(int numBol) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
