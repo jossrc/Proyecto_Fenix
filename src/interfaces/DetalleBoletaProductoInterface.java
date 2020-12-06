6b6532a00fc2b87ecf17297ce2bfeeca6e442412
jossrc <kuro.rc10@gmail.com>
2020-12-06 14:28:12 -0500

    Creando Interface para el DetalleBoletaProducto y agregando metodo para listado

---
+++ src/interfaces/DetalleBoletaProductoInterface.java
@@ -0,0 +1,11 @@
+package interfaces;
+
+import java.util.ArrayList;
+
+import model.DetalleBoleta;
+
+public interface DetalleBoletaProductoInterface {
+
+	public ArrayList<DetalleBoleta> listaDetalle();
+	
+}
